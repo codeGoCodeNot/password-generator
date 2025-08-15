2fc48eb21041036a6d0ca0290315b8156f070341
sendosend <johnsenberdin2930@gmail.com>
2025-08-15 10:04:24 +0800

    fixing the toggle github won't load the toggle mode

--- toggle.js
+++ toggle.js
@@ -1,14 +1,15 @@
-const toggle = () => {
-  const body = document.body;
-  const btn = document.querySelector('.toggle-btn');
-  btn.addEventListener('click', () => {
-    body.classList.toggle('dark');
-    body.classList.toggle('light');
+document.addEventListener('DOMContentLoaded', () => {
+  const toggle = () => {
+    const body = document.body;
+    const btn = document.querySelector('.toggle-btn');
+    btn.addEventListener('click', () => {
+      body.classList.toggle('dark');
+      body.classList.toggle('light');
 
-    body.classList.contains('light')
-      ? (btn.textContent = 'Dark mode')
-      : (btn.textContent = 'Light mode');
-  });
-};
-
-toggle();
+      body.classList.contains('light')
+        ? (btn.textContent = 'Dark mode')
+        : (btn.textContent = 'Light mode');
+    });
+  };
+  toggle();
+});
