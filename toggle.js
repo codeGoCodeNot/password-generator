5ef58ee289729a37c72087a30face9c3ec31deeb
sendosend <johnsenberdin2930@gmail.com>
2025-08-15 09:55:02 +0800

    toggle function

---
+++ toggle.js
@@ -0,0 +1,14 @@
+const toggle = () => {
+  const body = document.body;
+  const btn = document.querySelector('.toggle-btn');
+  btn.addEventListener('click', () => {
+    body.classList.toggle('dark');
+    body.classList.toggle('light');
+
+    body.classList.contains('light')
+      ? (btn.textContent = 'Dark mode')
+      : (btn.textContent = 'Light mode');
+  });
+};
+
+toggle();
