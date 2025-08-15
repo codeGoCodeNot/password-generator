90276585e05512bf5768cf6b26db68ac273779d7
sendosend <johnsenberdin2930@gmail.com>
2025-08-15 08:59:32 +0800

    adding reset function for new game

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
+const scoreHome = document.querySelector('.score-home');
+const scoreGuest = document.querySelector('.score-guest');
+
+let homeCount = 0;
+let guestCount = 0;
+
 const increment = () => {
-  const scoreHome = document.querySelector('.score-home');
-  const scoreGuest = document.querySelector('.score-guest');
   const btnHome1 = document.querySelector('#btn-home');
   const btnGuest1 = document.querySelector('#btn-guest');
   const btnHome2 = document.querySelector('#btn-home2');
   const btnGuest2 = document.querySelector('#btn-guest2');
   const btnHome3 = document.querySelector('#btn-home3');
   const btnGuest3 = document.querySelector('#btn-guest3');
-
-  let homeCount = 0;
-  let guestCount = 0;
 
   btnHome1.addEventListener('click', () => {
     homeCount++;
@@ -39,6 +40,19 @@ const increment = () => {
   btnGuest3.addEventListener('click', () => {
     guestCount += 3;
     scoreGuest.textContent = String(guestCount).padStart(2, '0');
+  });
+
+  reset();
+};
+
+const reset = () => {
+  const resetBtn = document.querySelector('#reset-btn');
+  resetBtn.addEventListener('click', () => {
+    homeCount = 0;
+    guestCount = 0;
+
+    scoreHome.textContent = '00';
+    scoreGuest.textContent = '00';
   });
 };
 
