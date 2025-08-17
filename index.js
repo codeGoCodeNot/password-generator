3023fd098ca39e7a5dccaac523fbac16a71ece05
sendosend <johnsenberdin2930@gmail.com>
2025-08-17 11:56:22 +0800

    Initial commit

--- index.js
+++ index.js
@@ -1,59 +1,28 @@
-const scoreHome = document.querySelector('.score-home');
-const scoreGuest = document.querySelector('.score-guest');
+function toggleMode() {
+  const btn = document.querySelector('.btn');
+  btn.addEventListener('click', (e) => {
+    e.preventDefault();
+    const section = document.querySelector('section');
+    const content = document.querySelector('.content');
+    const softLine = document.querySelector('.soft-line');
+    section.classList.toggle('dark');
+    section.classList.toggle('light');
+    btn.classList.toggle('dark');
+    btn.classList.toggle('light');
+    btn.textContent = section.classList.contains('dark')
+      ? 'Light mode'
+      : 'Dark mode';
 
-let homeCount = 0;
-let guestCount = 0;
+    content.classList.toggle('dark');
+    content.classList.toggle('light');
 
-const increment = () => {
-  const btnHome1 = document.querySelector('#btn-home');
-  const btnGuest1 = document.querySelector('#btn-guest');
-  const btnHome2 = document.querySelector('#btn-home2');
-  const btnGuest2 = document.querySelector('#btn-guest2');
-  const btnHome3 = document.querySelector('#btn-home3');
-  const btnGuest3 = document.querySelector('#btn-guest3');
-
-  btnHome1.addEventListener('click', () => {
-    homeCount++;
-    scoreHome.textContent = String(homeCount).padStart(2, '0');
+    softLine.classList.toggle('dark');
+    softLine.classList.toggle('light');
   });
+}
 
-  btnGuest1.addEventListener('click', () => {
-    guestCount++;
-    scoreGuest.textContent = String(guestCount).padStart(2, '0');
-  });
+function init() {
+  toggleMode();
+}
 
-  btnHome2.addEventListener('click', () => {
-    homeCount += 2;
-    scoreHome.textContent = String(homeCount).padStart(2, '0');
-  });
-
-  btnGuest2.addEventListener('click', () => {
-    guestCount += 2;
-    scoreGuest.textContent = String(guestCount).padStart(2, '0');
-  });
-
-  btnHome3.addEventListener('click', () => {
-    homeCount += 3;
-    scoreHome.textContent = String(homeCount).padStart(2, '0');
-  });
-
-  btnGuest3.addEventListener('click', () => {
-    guestCount += 3;
-    scoreGuest.textContent = String(guestCount).padStart(2, '0');
-  });
-
-  reset();
-};
-
-const reset = () => {
-  const resetBtn = document.querySelector('#reset-btn');
-  resetBtn.addEventListener('click', () => {
-    homeCount = 0;
-    guestCount = 0;
-
-    scoreHome.textContent = '00';
-    scoreGuest.textContent = '00';
-  });
-};
-
-increment();
+init();
